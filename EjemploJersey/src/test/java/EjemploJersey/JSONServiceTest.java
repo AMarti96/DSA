5637c4647f44fad5464f289ceb6eb3acc97bb2d2
AMarti96 <m4lua@hotmail.com>
2018-03-12 12:49:31 +0100

    first commit

---
+++ EjemploJersey/src/test/java/EjemploJersey/JSONServiceTest.java
@@ -0,0 +1,24 @@
+package EjemploJersey;
+
+import org.junit.Test;
+
+/**
+ * Created by juan on 16/11/16.
+ */
+public class JSONServiceTest {
+    @Test
+    public void getTrack() throws Exception {
+
+    }
+
+    @Test
+    public void getTrackInJSON() throws Exception {
+
+    }
+
+    @Test
+    public void createTrackInJSON() throws Exception {
+
+    }
+
+}
